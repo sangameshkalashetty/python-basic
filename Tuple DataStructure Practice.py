4506173c65b92eedf8191f0cd9a00d216f731908
sangameshkalashetty <sangameshsangameshkalshetty@gmail.com>
2024-11-13 17:34:40 +0530

    Add files via upload

---
+++ Tuple DataStructure Practice.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+t = () #Empty Tuple creation
+
+
+# In[2]:
+
+
+t
+
+
+# In[3]:
+
+
+type(t)
+
+
+# In[5]:
+
+
+tup2 =(10,20,30) # Tuple creation with int data type
+tup2
+
+
+# In[6]:
+
+
+tup3 = (1.2,5.4,10.3,200.9,9.9) # Tuple creation with float numbers
+tup3
+
+
+# In[7]:
+
+
+tup4 = ('one','two','three','four')
+tup4
+
+
+# In[8]:
+
+
+tup5 = ('Nit',11,(50,100),(150,90)) #Nested Tuples
+tup5
+
+
+# In[9]:
+
+
+tup6 = (100,'Asif','14.65')
+tup6
+
+
+# In[10]:
+
+
+tup7 = ('Asif',25,[50,100],[150,90],{'John','David'},(99,22,33))
+tup7
+
+
+# In[11]:
+
+
+len(tup7)
+
+
+# # Tuple Indexing
+
+# In[12]:
+
+
+tup2[0] #Retrives the first element of tuple
+
+
+# In[13]:
+
+
+tup4[0] #Retrieves the first element of tuple4
+
+
+# In[14]:
+
+
+tup4[0][0] # Nested Indexing- Access the first character of the first element
+
+
+# In[15]:
+
+
+tup4[-1]
+
+
+# In[18]:
+
+
+tup5[-1]  #Last item of the tuple
+
+
+# In[ ]:
+
+
+
+
